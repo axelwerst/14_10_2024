cde8160001608fec5f5b833ed8fda19c6d0dcf66
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-10-14 15:30:49 +0200

    no imput

--- src/main/java/InputMas.java
+++ src/main/java/InputMas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class InputMas {
     public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in) ;
+    /*Scanner scanner = new Scanner(System.in) ;
         System.out.println("imput a length of array");
         int lan = scanner.nextInt();
         System.out.println("imput a array with langth " + lan );
@@ -18,10 +18,10 @@ public class InputMas {
         }
         }
         scanner.close();
-        System.out.println("yours array" + Arrays.toString(array));
-       // int[] array = {0,1,2,3,4,5,6,7,8,9,10} ;
-       // int lan = array.length;
-         k=0;
+        System.out.println("yours array" + Arrays.toString(array)); */
+        int[] array = {0,1,2,3,4,5,6,7,8,9,10} ;
+       int lan = array.length;
+         int k=0;
         while (k<=lan) {
             if (array[k]%2 < 1) {
                 System.out.println(array[k]);
